04f7399e568f003b25fe421dac08d44f79abf937
mknightjre <mknightjr@gmail.com>
2014-03-03 22:52:03 -0500

    First commit, nameCheck.rb

---
+++ nameCheck.rb
@@ -0,0 +1,15 @@
+names = ["mike", "shawn", "john", "don"]
+
+puts "what's your first name?"
+user_name = gets.chomp
+
+names.each do |name| 
+if name == user_name
+  puts "your name is " + "#{user_name}" +"!"
+else 
+  puts "I don't know you, but nice to meet you!"
+end
+end
+
+
+
